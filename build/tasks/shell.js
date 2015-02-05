12d1df99c5a7ee69b51f5cff4690a2ad08e9bf3b
Matt Surabian <matt@mattsurabian.com>
2015-02-05 10:08:04 -0500

    Rename episodes S3 bucket

--- build/tasks/shell.js
+++ build/tasks/shell.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
             command: 'aws --profile rubberduck s3 cp _site/ s3://rubberduckcast.com --recursive --include \'*\' --acl \'public-read\''
         },
         push_episodes: {
-            command: 'aws --profile rubberduck s3 cp _audio/ s3://rubberduckingepisodes --recursive --include \'*\' --acl \'public-read\''
+            command: 'aws --profile rubberduck s3 cp _audio/ s3://episodes.rubberduckcast.com --recursive --include \'*\' --acl \'public-read\''
         },
         download_report: {
             command: 'aws --profile rubberduck s3 sync s3://logs-rubberducking ./_s3_logs --recursive && ' +
